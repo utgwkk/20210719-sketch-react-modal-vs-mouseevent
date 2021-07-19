2bb0ad0ef13778c63a5ee1e448109af973d9f67b
utgwkk <utagawakiki@gmail.com>
2021-07-19 10:51:00 +0900

    modal inside draggable component

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,4 +1,5 @@
-import { MouseEventHandler, useCallback } from "react";
+import { MouseEventHandler, useCallback, useState } from "react";
+import Modal from "react-modal";
 
 import styles from "./Draggable.module.css";
 
@@ -6,10 +7,22 @@ export const Draggable = () => {
   const handleMouseDown: MouseEventHandler = useCallback((e) => {
     console.log(`clicked!!!! ${new Date()}`);
   }, []);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleRequestClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
 
   return (
     <div className={styles.container} onMouseDown={handleMouseDown}>
       This area is draggable!!!!!
+      <button onClick={() => setIsOpen(true)}>open modal</button>
+      <Modal isOpen={isOpen} onRequestClose={handleRequestClose}>
+        <div>
+          <h2>Modal</h2>
+          <button onClick={handleRequestClose}>close</button>
+        </div>
+      </Modal>
     </div>
   );
 };
