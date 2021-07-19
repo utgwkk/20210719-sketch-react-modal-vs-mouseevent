387b0f677cacaaa80d8ada51c04278732d8a0aba
utgwkk <utagawakiki@gmail.com>
2021-07-19 10:42:24 +0900

    handle mousedown event

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -1,5 +1,15 @@
+import { MouseEventHandler, useCallback } from "react";
+
 import styles from "./Draggable.module.css";
 
 export const Draggable = () => {
-  return <div className={styles.container}>This area is draggable!!!!!</div>;
+  const handleMouseDown: MouseEventHandler = useCallback((e) => {
+    console.log(`clicked!!!! ${new Date()}`);
+  }, []);
+
+  return (
+    <div className={styles.container} onMouseDown={handleMouseDown}>
+      This area is draggable!!!!!
+    </div>
+  );
 };
