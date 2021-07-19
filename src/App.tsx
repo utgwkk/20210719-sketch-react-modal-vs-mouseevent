c43658a0013ef0bfdff96fe1775efff8f9b1bdd0
utgwkk <utagawakiki@gmail.com>
2021-07-19 12:21:52 +0900

    children

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { useCallback, useState } from "react";
+import Modal from "react-modal";
+
 import { Clickable } from "./Clickable";
 
 export const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleRequestClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello, world!</h1>
-      <Clickable />
+      <Clickable>
+        This area is clickable!!!!!
+        <button onClick={() => setIsOpen(true)}>open modal</button>
+        <Modal isOpen={isOpen} onRequestClose={handleRequestClose}>
+          <div>
+            <h2>Modal</h2>
+            <button onClick={handleRequestClose}>close</button>
+          </div>
+        </Modal>
+      </Clickable>
     </div>
   );
 };
--- src/Clickable.tsx
+++ src/Clickable.tsx
@@ -1,28 +1,15 @@
-import { MouseEventHandler, useCallback, useState } from "react";
-import Modal from "react-modal";
+import { FC, MouseEventHandler, useCallback } from "react";
 
 import styles from "./Clickable.module.css";
 
-export const Clickable = () => {
+export const Clickable: FC = ({ children }) => {
   const handleMouseDown: MouseEventHandler = useCallback((e) => {
     console.log(`clicked!!!! ${new Date()}`);
-  }, []);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleRequestClose = useCallback(() => {
-    setIsOpen(false);
   }, []);
 
   return (
     <div className={styles.container} onMouseDown={handleMouseDown}>
-      This area is clickable!!!!!
-      <button onClick={() => setIsOpen(true)}>open modal</button>
-      <Modal isOpen={isOpen} onRequestClose={handleRequestClose}>
-        <div>
-          <h2>Modal</h2>
-          <button onClick={handleRequestClose}>close</button>
-        </div>
-      </Modal>
+      {children}
     </div>
   );
 };
