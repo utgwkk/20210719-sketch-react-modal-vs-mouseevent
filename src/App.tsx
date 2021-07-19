90223f1dc9248a9b0e53f05bc68eb13307af3b45
utgwkk <utagawakiki@gmail.com>
2021-07-19 10:39:57 +0900

    draggable area

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Draggable } from "./Draggable";
+
 export const App = () => {
   return (
     <div className="App">
       <h1>Hello, world!</h1>
+      <Draggable />
     </div>
   );
 };
---
+++ src/Draggable.tsx
@@ -0,0 +1,5 @@
+import styles from "./Draggable.module.css";
+
+export const Draggable = () => {
+  return <div className={styles.container}>This area is draggable!!!!!</div>;
+};
